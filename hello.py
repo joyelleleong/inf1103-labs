49910ed0558de0370de6fec2e58440e3eac56f08
Joyelle <joyelleisme@gmail.com>
2026-09-04 14:42:53 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
+print ("====================")
 print ("welcome here")
+print ("My first post!")
+print ("====================")
+
+username = "cool_creator"
+bio = "fun blogger"
+followers = 1000
+
+print ("Username:", username)
+print ("Bio:", bio)
+print ("Followers:", followers) 
