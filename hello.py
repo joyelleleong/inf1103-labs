e3f9e87f95c75b0f2c94e378f867745815132546
Joyelle <joyelleisme@gmail.com>
2026-09-04 14:46:23 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,16 @@ print ("====================")
 
 username = "cool_creator"
 bio = "fun blogger"
-followers = 1000
+followers = 100
+
+followers += 50
+print ("Day 1:", followers)
+
+followers += 20
+print ("Day 2:", followers)
+
+followers -= 10
+print ("Day 3:", followers)
 
 print ("Username:", username)
 print ("Bio:", bio)
